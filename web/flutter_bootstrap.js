292267bfed7a19f807379b10ae12b3ac54867c0e
Anisimov Artem <anisimov.studio.work@gmail.com>
2025-12-28 21:47:13 +0300

    Revert "fix: Отключен Service Worker для работы через HTTP"
    This reverts commit 1b3ff4f3a990b5bf8fcffc31f95b5d26c036dfd3.

--- web/flutter_bootstrap.js
+++
@@ -1,9 +0,0 @@
-{{flutter_js}}
-{{flutter_build_config}}
-
-_flutter.loader.load({
-  // Отключаем Service Worker для работы через HTTP
-  serviceWorkerSettings: {
-    serviceWorkerVersion: null
-  }
-});
