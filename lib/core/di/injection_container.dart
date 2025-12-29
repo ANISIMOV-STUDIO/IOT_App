0f17d5930070ed091e7fad1e61bf130a6edc9a09
Anisimov Artem <anisimov.studio.work@gmail.com>
2025-12-29 03:38:11 +0300

    Update injection_container.dart

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -62,7 +62,7 @@ final sl = GetIt.instance;
 ///
 /// Установите в false для разработки UI без backend или для тестирования
 /// Установите в true для работы с реальным backend (https://89.207.223.45)
-const bool USE_REAL_API = true;
+const bool USE_REAL_API = false;
 
 /// Инициализация всех зависимостей
 Future<void> init() async {
