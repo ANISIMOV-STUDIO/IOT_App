9fd90905d8374917d1c4d3a105c30acc17dd9cf5
Kirill <meelstorm90@gmail.com>
2026-01-11 19:33:37 +0300

    fix: Убрана ложная плашка оффлайн в дашборде

--- lib/presentation/screens/dashboard/dashboard_screen.dart
+++ lib/presentation/screens/dashboard/dashboard_screen.dart
@@ -209,7 +209,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
             onTimerSettingsChanged: _handleTimerSettingsChanged,
             activeAlarms: data.climateState.activeAlarms,
             onAlarmsSeeHistory: () => context.goToAlarmHistory(unit.id, unit.name),
-            isOnline: data.climateState.isOnline,
+            isOnline: unit.isOnline,
             isPendingTemperature: data.climateState.isPendingTemperature,
           ),
         ),
